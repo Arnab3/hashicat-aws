7c52a77481c8fc706e95c7f023cee4aaaf8a8ac4
Arnab3 <arnabgogoi4k@gmail.com>
2022-12-06 10:08:18 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,3 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/Arnab/s3-bucket/aws"
   version = "3.6.0"
 }
+credentials "app.terraform.io" {
+    # valid user API token:
+    token = "xxxxxx.atlasv1.zzzzzzzzzzzzz"
+  }
